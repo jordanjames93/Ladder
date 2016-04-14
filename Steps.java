f3e7c48ab494e89a5d0ef023a85f48aec2dc21c4
jordanjames93 <jordan.byrne87@mail.dcu.ie>
2016-04-14 14:04:55 +0100

    Error-handling getSteps()
    The array was replaced by a TreeSet.
    The constructor didn't need to initialise the array so was altered slightly.
    The getSteps() method was altered to check that the input was in fact a number.
    The getSteps() method added the values to the new TreeSet.
    
    The reason for these changes was to ensure an ordered, un-repeating set of integers was created and ready for use in the findLargestGap() method.

--- Steps.java
+++ Steps.java
@@ -1,27 +1,51 @@
 import java.util.Scanner;
+import java.util.TreeSet;
 
 class Ladder {
 	private int numSteps;
-	private int[] steps;
+	private TreeSet<Integer> steps = new TreeSet<Integer>(); // Keeps the data sorted. Removes duplicates.
 	private int largestGap = 0;
 	private Scanner input = new Scanner(System.in);
 	
 	Ladder(int numSteps) {
 		this.numSteps = numSteps;
-		steps = new int[numSteps];
 	}
 	
 	private void getSteps() {
-		System.out.println("Enter the " + numSteps + " step heights: ");
-		for(int i = 0; i < steps.length; i++) {
-			steps[i] = input.nextInt();
+		System.out.println("Enter the " + numSteps + " step heights(number greater than 0): ");
+		for(int i = 0; i < numSteps; i++) {
+			// Check that input is a number
+			if(!input.hasNextInt()) {
+				input.next();
+				i--;
+				continue;
+			}
+			// check that the number is greater than 0
+			int height = input.nextInt();
+			if(height <= 0) {
+				input.next();
+				i--;
+				continue;
+			}
+			// add to set
+			steps.add(height);
 		}
 	}
 	
+	
+//	private void findLargestGap() {
+//		int currGap = 0;
+//		for(int i = 1; i < steps.length - 1; i++) {
+//			currGap = steps[i] - steps[i - 1];
+//			if(currGap > largestGap) largestGap = currGap;
+//		}
+//	}
+	
 	private void findLargestGap() {
 		int currGap = 0;
-		for(int i = 1; i < steps.length - 1; i++) {
-			currGap = steps[i] - steps[i - 1];
+		Object [] stepsArray = steps.toArray();
+		for(int i = 1; i < stepsArray.length; i++) {
+			currGap = (int)stepsArray[i] - (int)stepsArray[i - 1];
 			if(currGap > largestGap) largestGap = currGap;
 		}
 	}
